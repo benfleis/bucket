732721cab6c76ccbe4bc434b2545e8a931316d25
ben fleis <ben@monkey.org>
2013-06-21 17:32:52 +0200

    add delete script creation before i change the basic mechanism to have a 'keeper' directory

--- scripts/duplicates.py
+++ scripts/duplicates.py
@@ -23,13 +23,11 @@ import collections
 import fnmatch
 import hashlib
 import json
-#import logging
 import os
 import pipes    # for pipes.quote, shell escaping: http://stackoverflow.com/questions/35817/how-to-escape-os-system-calls-in-python
 import re
 import sys
 
-#from logging import debug, info, warning, error
 from os.path import getsize, join, splitext
 
 # each key is as named, and each entry is a string or a set of 'full paths',
@@ -40,7 +38,7 @@ from os.path import getsize, join, splitext
 by_ext = collections.defaultdict(lambda:
     { 'by_name': {}, 'by_size': {}, 'by_partial_hash': {}, 'by_hash': {}, },)
 
-def update_by_name_and_by_size(path, exclude, include):
+def update_by_name_and_by_size(path, exclude=None, include=None, delete=False):
     for root, dirs, files in os.walk(unicode(path)):
         if exclude:
             # filter excluded dirs
@@ -86,27 +84,27 @@ def update_by_name_and_by_size(path, exclude, include):
             if bsv is not full:
                 if type(bsv) is unicode:
                     bev['by_size'][size] = set((bsv, full))
-                    update_by_partial_hash(bev, bsv)
+                    update_by_partial_hash(bev, bsv, delete)
                 if type(bsv) is set:
                     bsv.add(full)
-                update_by_partial_hash(bev, full)
+                update_by_partial_hash(bev, full, delete)
         print
 
 
-def update_by_partial_hash(bev, full):
+def update_by_partial_hash(bev, full, delete):
     try: hash = hashlib.md5(open(full, 'rb').read(512)).hexdigest()
     except: return
     bphv = bev['by_partial_hash'].setdefault(hash, full)
     if bphv is not full:
         if type(bphv) is unicode:
             bev['by_partial_hash'][hash] = set((bphv, full))
-            update_by_hash(bev, bphv)
+            update_by_hash(bev, bphv, delete)
         if type(bphv) is set:
             bphv.add(full)
-        update_by_hash(bev, full)
+        update_by_hash(bev, full, delete)
 
 
-def update_by_hash(bev, full):
+def update_by_hash(bev, full, delete):
     try: input = open(full, 'rb', 64 * 1024)
     except: return
     md5 = hashlib.md5()
@@ -116,10 +114,13 @@ def update_by_hash(bev, full):
     bhv = bev['by_hash'].setdefault(hash, full)
     if bhv is not full:
         if type(bhv) is unicode:
-            bev['by_hash'][hash] = set((bhv, full))
-        if type(bhv) is set:
-            bhv.add(full)
-
+            bev['by_hash'][hash] = [bhv, full]
+            if delete:
+                delete.append(full, bhv)
+        if type(bhv) is list:
+            bhv.append(full)
+            if delete:
+                delete.append(bhv[-1], bhv[0])
 
 # -----------------------------------------------------------------------------
 
@@ -128,18 +129,33 @@ def main(args):
     p = argparse.ArgumentParser(description='look for duplicate files')
     p.add_argument('--exclude', action='append', help='exclude glob pattern')
     p.add_argument('--include', action='append', help='include glob pattern')
-    #p.add_argument('-v', '--verbose', dest='verbose', default=0, action='count', help='verbose mode')
-    p.add_argument('path', nargs='+', help='paths to search')
+    p.add_argument('--delete-script', dest='delete_script', help=''.join([
+        'Generate shell script to delete all duplicates found in 2nd, 3rd, ',
+        'etc., specified paths, placed in named file.  Must specify more ',
+        'than one path.']))
+    p.add_argument('paths', nargs='+', help='paths to search')
     args = p.parse_args(args[1:])
+    if (args.delete_script and len(args.paths) < 2):
+        p.print_help(sys.stderr)
+        return 1
+    if len(args.paths) != len(set(args.paths)):
+        sys.stderr.write('Redundant PATHS found: {0}\n\n'.format(args.paths))
+        p.print_usage(sys.stderr)
+        return 1
 
     # convert set of globs into a singular RE
     exclude = None if not args.exclude else re.compile('(?:{0})'.format(
         ')|(?:'.join([fnmatch.translate(ex) for ex in args.exclude])))
     include = None if not args.include else re.compile('(?:{0})'.format(
         ')|(?:'.join([fnmatch.translate(ex) for ex in args.include])))
+    delete = None if not args.delete_script else DeleteScriptGenerator()
 
-    for path in args.path:
-        update_by_name_and_by_size(path, exclude, include)
+    update_by_name_and_by_size(args.paths[0], exclude, include, None)
+    for path in args.paths[1:]:
+        update_by_name_and_by_size(path, exclude, include, delete)
+
+    if delete:
+        delete.write(args.delete_script)
 
     # save raw map as json to .duplicates.json
     class JsonSetEncoder(json.JSONEncoder):
@@ -155,7 +171,7 @@ def main(args):
     print '\nby hash:'
     for ext, maps in by_ext.iteritems():
         for hash, fullnames in maps['by_hash'].iteritems():
-            if type(fullnames) is set:
+            if type(fullnames) is list:
                 print u'{0}: {1}'.format(hash, u' '.join(map(pipes.quote, fullnames)))
                 already.update(fullnames)
 
@@ -168,6 +184,35 @@ def main(args):
                     print u'{0}: {1}'.format(name, u' '.join(map(pipes.quote, notyet)))
                     already.update(notyet)
 
+
+# -----------------------------------------------------------------------------
+
+class DeleteScriptGenerator:
+    def __init__(self):
+        self.header = '#!/bin/sh\n\n'
+        self.lines = []
+        self.line_max = 0
+
+    def append(self, full, duplicate_of=None):
+        line = 'rm {0}'.format(pipes.quote(full))
+        self.lines.append((line, duplicate_of))
+        self.line_max = max(self.line_max, len(line))
+
+    def write(self, filename):
+        if not self.lines:
+            return
+
+        out = open(filename, 'w')
+        off = ((self.line_max + 4) / 4) * 4
+        fmt = '{0:' + str(off) + 's}# {1}\n'
+        for line, duplicate_of in self.lines:
+            if duplicate_of:
+                out.write(fmt.format(line, duplicate_of))
+            else:
+                out.write('{0}\n'.format(line))
+        out.close()
+
+# -----------------------------------------------------------------------------
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv))
